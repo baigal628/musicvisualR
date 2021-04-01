57bead727dbdffb98736d4447936452829251882
baigal628 <gali.bai@hotmail.com>
2021-04-01 14:29:30 -0500

    Construct project structure
    1. renv::init() for dependencies version control.
    2. renv::snapshot() to store dependencies information in renv.lock.
    3. usethis::use_readme_rmd() to add README.Rmd

--- R/initialization.R
+++ R/initialization.R
@@ -2,4 +2,16 @@ library(devtools)
 packageVersion("devtools")
 library(tidyverse)
 library(fs)
-use_git()
+#use_git()
+
+dir_info()
+#renv::init()
+## Use this to update lockfile every time import a new package
+renv::snapshot()
+## Use this to update package
+#renv::update()
+## Use this when moving to new computer
+#renv::restore()
+
+#usethis::use_package(package = "renv", type = "Suggests")
+#usethis::use_readme_rmd()
